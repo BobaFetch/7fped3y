89506fd53d50b400b8461af43988609824dd2798
bobaFetch <thejza@Andrews-MacBook-Air.local>
2022-04-30 20:43:02 -0700

    fix?

--- src/routes/api/creators.js
+++ src/routes/api/creators.js
@@ -1,18 +1,6 @@
 import db from '$lib/db';
-import { supabase } from '$lib/supabase';
 
 export async function get() {
-	const { error, data } = await supabase.from('contacts').select(`
-		*,
-		users (
-			company_id,
-			team_id
-		)
-	`);
-
-	if (data) {
-		console.log(data);
-	}
 	const creators = db
 		.prepare(
 			'SELECT contacts.*, company_id team_id FROM contacts JOIN users ON users.user_id = contacts.owner_id'
