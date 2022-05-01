8c5fc1ac874cdb0e0f2c8e1aea9c8e884b3da305
bobaFetch <thejza@Andrews-MacBook-Air.local>
2022-05-01 15:56:29 -0700

    added spinner to login

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -1,11 +1,22 @@
 <script>
   import {fly} from 'svelte/transition'
   import { goto } from '$app/navigation'; 
+  import { Icon } from '@steeze-ui/svelte-icon'
+  import { Loader } from '@steeze-ui/feather-icons'
 
   import {authenticated} from '$lib/stores/tempStore'
   export let newSignUp
   let email
   let password
+
+  let loadingModal = true
+
+  const handleDelay = () => {
+    loadingModal = false
+    setTimeout(() => {
+      handleLogin()
+    }, 2000)
+  }
 
   const handleSignUp = () => {
     newSignUp = true
@@ -17,10 +28,18 @@
   }
 </script>
 
-<div in:fly={{x:200, duration: 200}} class='flex flex-col justify-center items-center w-full mb-5'>
-  <h1 class='text-brandWhite text-center text-4xl sm:text-5xl md:text-6xl italic font-bold mb-5'>Login</h1>
-  <input type="email" bind:value={email} placeholder="ex: name@work-email.com" class="w-1/2 mt-5 mb-2 p-3 rounded-lg bg-[#ddf0f2]" />
-  <input type="password" bind:value={password} placeholder="" class="w-1/2 mb-5 p-3 rounded-lg bg-[#ddf0f2]" />
-  <button class="bg-[#52c0cc] w-1/2 p-3 rounded-lg" on:click|preventDefault={handleLogin}>Submit</button>
+<div class:blur-sm={!loadingModal}>
+  <div in:fly={{x:200, duration: 200}} class='flex flex-col justify-center items-center w-full mb-5'>
+    <h1 class='text-brandWhite text-center text-4xl sm:text-5xl md:text-6xl italic font-bold mb-5 font-header'>Login</h1>
+    <input type="email" bind:value={email} placeholder="ex: name@work-email.com" class="w-1/2 mt-5 mb-2 p-3 rounded-lg bg-[#ddf0f2]" />
+    <input type="password" bind:value={password} placeholder="" class="w-1/2 mb-5 p-3 rounded-lg bg-[#ddf0f2]" />
+    <button class="bg-[#52c0cc] w-1/2 p-3 rounded-lg" on:click|preventDefault={handleDelay}>Submit</button>
+  </div>
+  <p class="text-brandWhite text-center">Need an account? <span on:click|preventDefault={handleSignUp} class=" text-[#6ecad4] cursor-pointer">Sign Up</span></p>
 </div>
-<p class="text-brandWhite text-center">Need an account? <span on:click|preventDefault={handleSignUp} class=" text-[#6ecad4] cursor-pointer">Sign Up</span></p>
+
+<div class:hidden={loadingModal} class="absolute top-0 left-0 w-full h-full">
+  <div class="flex items-center justify-center h-full">
+      <Icon src={Loader} width="100" height="100" class="text-teal-500 animate-spin-slow" />
+  </div>
+</div>
