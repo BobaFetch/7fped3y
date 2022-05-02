aa7c2d7aa2966a14af8d8f9c1be54f15aa1f27e2
bobaFetch <thejza@Andrews-MacBook-Air.local>
2022-05-02 11:08:49 -0700

    removed excess from team card

--- src/lib/components/TeamCard.svelte
+++ src/lib/components/TeamCard.svelte
@@ -27,7 +27,7 @@
         member.role = selectedValue
         console.log(member)}
       } 
-      class="bg-slate-900 text-xs mr-2 col-span-2 col-start-5">
+      class="bg-slate-900 text-xs mr-2 col-span-2 col-start-4">
       <option value={member.role}>{member.role}</option>
       {#if member.role === 'Editor'}
         <option value={'Admin'}>Admin</option>
@@ -38,8 +38,8 @@
   {:else}
     <p class="text-xs col-span-1 col-start-5">{member.role}</p>
   {/if}
-    <p class="text-xs col-span-2 col-start-7">{member.email}</p>
-    <p class="text-xs col-span-2 col-start-10 md:visible invisible">{member.phone ? member.phone : 'XXX-XXX-XXXX'}</p>
+    <p class="text-xs col-span-2 col-start-7 sm:block hidden">{member.email}</p>
+    <p class="text-xs col-span-2 col-start-10 md:block hidden">{member.phone ? member.phone : 'XXX-XXX-XXXX'}</p>
     {#if user.role === 'Admin'}
       <input type="button" value="Remove" class="text-red-500 text-xs col-span-1 col-start-12 cursor-pointer" on:click={() => dispatch('delete')}/>
     {/if}
